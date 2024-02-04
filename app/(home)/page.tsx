fca31f1dc45601af670bf5d2395ac360ddb4ab53
viniciusrvieira <vinicius.vieira@buscaorganica.com.br>
2024-02-04 17:05:15 -0300

    feat: Welcome menssage

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,10 +1,17 @@
-import Image from "next/image";
+import { ptBR } from "date-fns/locale";
 import Header from "../_components/header";
+import { format } from 'date-fns'
 
 export default function Home() {
-  return ( 
-    <div className="">
-        <Header/>
+  return (
+    <div>
+      <Header />
+      <div className="px-5 pt-5">
+        <h2 className="text-xl font-bold">Olá, Miguel! </h2>
+        <p className="capitalize text-sm">{format(new Date(), "EEEE',' d 'de' MMMM", {
+          locale: ptBR
+        })}</p>
+      </div>
     </div>
-   );
+  );
 }
