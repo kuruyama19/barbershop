29e372ebffcf38f7a94e8369a267d15da0750c5a
viniciusrvieira <vinicius.vieira@buscaorganica.com.br>
2024-02-05 14:17:23 -0300

    feat:add nav detais barber

---
+++ app/barbershops/[id]/page.tsx
@@ -0,0 +1,27 @@
+import { db } from "@/app/_lib/prisma";
+
+interface BarbershopDetailsPageProps {
+    params: {
+        id?: string
+    }
+}
+
+const BarbershopDetailsPage = async ({ params }: BarbershopDetailsPageProps) => {
+    if (!params.id) {
+        // TODO: redirecionar para home
+        return null;
+    }
+    const barbershop = await db.barbershop.findUnique({
+        where: {
+            id: params.id
+        }
+    })
+    if (!barbershop) {
+        return null
+    }
+    return (
+        <h1>{barbershop.name}</h1>
+    );
+}
+
+export default BarbershopDetailsPage;
