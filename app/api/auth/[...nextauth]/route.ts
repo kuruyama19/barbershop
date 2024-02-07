f0ef1bc699c996d059bb399c9c9e3bd021e256cc
viniciusrvieira <vinicius.vieira@buscaorganica.com.br>
2024-02-07 20:57:08 -0300

    feat:setup next auth

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -8,8 +8,8 @@ import { Adapter } from "next-auth/adapters";
 const handler = NextAuth({
     adapter: PrismaAdapter(db) as Adapter,
   providers: [GoogleProvider ({
-    clientId:"",
-    clientSecret:"",
+    clientId: process.env.GOOGLE_CLIENT_ID as string,
+    clientSecret:process.env.GOOGLE_CLIENT_SECRET as string,
   })]
 })
 
